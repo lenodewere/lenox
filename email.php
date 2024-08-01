c7bbbe78ff36df9784de1e4d1253a0fa0cb80c08
Were Lenox <104990838+lenodewere@users.noreply.github.com>
2024-08-01 16:22:38 +0300

    Lennie
    Lennie

--- email.php
+++ email.php
@@ -1,14 +1,8 @@
+
 <?php
-
-if (isset($_POST['submit'])) {
-  $to = "lenoxochieng903@gmail.com";
-  $subject = "Contact Form Submission";
-  $message = "Name: " . $_POST['name'] . "\n";
-  $message .= "Email: " . $_POST['email'] . "\n";
-  $message .= "Message: " . $_POST['message'] . "\n";
-
-  // Send the email
-  mail($to, $subject, $message);
-}
-
+$to = 'recipient-email@example.com';
+$subject = 'Email Subject';
+$message = 'Email Body';
+$headers = 'From: your-email@example.com';
+mail($to, $subject, $message, $headers);
 ?>
